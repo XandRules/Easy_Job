dd52632883396bf8154f0474888128e4c800a4cd
XandRules <alexandresilva58@gmail.com>
2020-09-23 16:08:47 -0300

    corrigido evento de alteração de arquivo

--- js/controller/freelancer.controller.js
+++ js/controller/freelancer.controller.js
@@ -19,7 +19,7 @@ easyjob.controller('FreelancerController', [
 
     let sessionValidated = JSON.parse(sessionStorage.getItem('sessionValidated'));
 
-    $rootScope.name = sessionValidated.freelancer.name.split(" ")[0];
+    $rootScope.name = sessionValidated != undefined ? sessionValidated.freelancer.name.split(" ")[0] : null;
 
     $scope.imageUploadPreview = function () {
 
@@ -50,5 +50,7 @@ easyjob.controller('FreelancerController', [
 
       }
     }
+
+    let event = document.querySelector('#file-input') != null ? document.querySelector('#file-input').addEventListener("change", $scope.imageUploadPreview) : null;
   }
 ]);
