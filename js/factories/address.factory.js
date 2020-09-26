33213d574e8b2218d965fa092120a381757c8c80
xandrules <alexandresilva58@gmail.com>
2020-09-26 13:38:24 -0500

    erro token

--- js/factories/address.factory.js
+++ js/factories/address.factory.js
@@ -7,8 +7,7 @@ easyjob.factory('AddressModel', [
 
     AddressFactory.getAddressById = function(id){
 
-      config.defaultHeaderToken.Authorization += $rootScope.token;
-
+     config.defaultHeaderToken.Authorization = 'Bearer ' + $rootScope.token;
 
      return $http.get(config.baseUrl + `/address/${id}`, config.defaultHeaderToken);
 
