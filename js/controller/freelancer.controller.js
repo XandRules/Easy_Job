fd2c0f94adbae3bfecf5498d16d9aba83b514199
xandrules <alexandresilva58@gmail.com>
2020-09-26 20:40:09 -0500

    deploy

--- js/controller/freelancer.controller.js
+++ js/controller/freelancer.controller.js
@@ -252,11 +252,12 @@ easyjob.controller('FreelancerController', [
     }
 
     if($rootScope.pageSelect == 'profilefreelancer'){
-      $scope.buscaUFs(1);
-      $scope.getFreelancerById();
-      document.getElementById("loading").style.display = "block";
-      document.getElementById("content").style.display = "none";
-      document.getElementById("content2").style.display = "none";
+      $scope.buscaUFs(1).then(response =>{
+        $scope.getFreelancerById();
+        document.getElementById("loading").style.display = "block";
+        document.getElementById("content").style.display = "none";
+        document.getElementById("content2").style.display = "none";
+      });
     }
 
     //SELECT  F.id, F.name, F.phone, F.speciality_id, F.birth, F.gender, F.cpf,F.bio, A.number, A.cep, A.public_place, A.uf, A.neighborhood, A.city FROM freelancers F INNER JOIN addresses A on A.freelancer_id = F.id where F.id = 9;
