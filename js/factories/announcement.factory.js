f8c998071e5f32a17d883d967e5a086c241a9ca4
xandrules <alexandresilva58@gmail.com>
2020-09-27 19:47:36 -0500

    deploy

--- js/factories/announcement.factory.js
+++ js/factories/announcement.factory.js
@@ -6,46 +6,30 @@ easyjob.factory('AnnouncementModel', [
 
     let token = $rootScope.token;
 
-    // axios.interceptors.request.use(
-    //   config =>{
-    //     config.headers.authorization = token;
-    //     return config;
-    //   },
-    //   error =>{
-    //     return Promisse.reject(error);
-    //   }
-    // );
+    axios.interceptors.request.use(
+      config =>{
+        config.headers.authorization = token;
+        return config;
+      },
+      error =>{
+        return Promisse.reject(error);
+      }
+    );
     var AnnoucementFactory = {};
 
     AnnoucementFactory.createAnnoucement = function (data) {
-      //return axios.post(config.baseUrl + '/announcement',data);
 
-      return axios.post(config.baseUrl + `/announcement`, {
-        headers: {
-          'Authorization': `Bearer ${$rootScope.token}`,
-          'Content-Type': 'application/json'
-        }
-      },data);
+      return axios.post(config.baseUrl + '/announcement',data);
     };
 
 
     AnnoucementFactory.getAnnouncementsFromFreelancer = function () {
-      return axios.get(config.baseUrl + `/announcements/${$rootScope.id}`,{
-        headers: {
-          'Authorization': `Bearer ${$rootScope.token}`,
-          'Content-Type': 'application/json'
-        }
-      });
+      return axios.get(config.baseUrl + `/announcements/${$rootScope.id}`);
     };
 
     AnnoucementFactory.deleteAnnouncementById = function (id) {
 
-      return axios.delete(config.baseUrl + `/announcements/${id}`, {
-        headers: {
-          'Authorization': `Bearer ${$rootScope.token}`,
-          'Content-Type': 'application/json'
-        }
-      });
+      return axios.delete(config.baseUrl + `/announcements/${id}`);
     };
 
     return AnnoucementFactory; 
