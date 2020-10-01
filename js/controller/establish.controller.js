83c3eeb06f1a132a3a9046260f4ffd7988aabda7
xandrules <alexandresilva58@gmail.com>
2020-09-30 19:51:40 -0500

    deploy

--- js/controller/establish.controller.js
+++ js/controller/establish.controller.js
@@ -4,27 +4,7 @@ easyjob.controller('EstablishController', ['EstablishModel', '$scope', '$rootSco
         $rootScope.headerDefault = false;
         $rootScope.headerDefaultLogout = true;
         $rootScope.footerDefault = false;
-        $scope.speciality = "";
-
-        $scope.getSpecilities = function () {
-
-            MainModel.getSpecilities().then(function (response) {
-      
-              $rootScope.specilities = [];
-      
-              response.data.forEach((element) => {
-                $rootScope.specilities.push(element);
-              });
-      
-              console.log($scope.specilities);
-              $scope.loading = angular.element('#loading').removeClass("loader loader-default is-active");
-            });
-      
-        };
 
 
-        if($rootScope.pageSelect == 'searchfreelancer'){
-            $scope.getSpecilities();
-        }
     }
 ]);
--- js/controller/search.controller.js
+++ js/controller/search.controller.js
@@ -1,7 +1,60 @@
 easyjob.controller('SearchController', [
   'SearchModel',
   '$scope',
-  function (EstablishModel, $scope) {
+  '$rootScope',
+  'MainModel',
+  function (EstablishModel, $scope,$rootScope,MainModel) {
     console.log('Search');
+
+    $rootScope.headerDefault = false;
+    $rootScope.headerDefaultLogout = true;
+    $rootScope.footerDefault = false;
+    $scope.speciality_label = ["Freelancer","Garçom / Garçonete","Segurança","Recepcionista","Banda / Músico", "Motoboy"];
+    $scope.speciality_label_writer = "";
+
+    $scope.speciality = "";
+
+        $scope.getSpecilities = function () {
+
+            MainModel.getSpecilities().then(function (response) {
+      
+              $rootScope.specilities = [];
+      
+              response.data.forEach((element) => {
+                $rootScope.specilities.push(element);
+              });
+      
+              console.log($scope.specilities);
+              $scope.loading = angular.element('#loading').removeClass("loader loader-default is-active");
+            });
+      
+        };
+
+        $scope.typeWriter = function(){
+
+          for (let i = 0; i < $scope.speciality_label.length; i++) {
+            
+            let writer = $scope.speciality_label[i].split("");
+          
+
+              writer.forEach((letter,j) => {
+                setTimeout(()=>{
+                  $scope.speciality_label_writer += writer;   
+                  console.log($scope.speciality_label_writer);    
+  
+                },100 * j)            
+              });     
+            
+          }
+
+          $scope.typeWriter();
+            
+        }
+
+
+        if($rootScope.pageSelect == 'searchfreelancer'){
+            $scope.getSpecilities();
+            $scope.typeWriter();
+        }
   },
 ]);
