53d118e069c4a03c41c721c487db69db307890d5
XandRules <alexandresilva58@gmail.com>
2020-10-02 15:36:26 -0300

    add token para estabelecimento

--- js/controller/establish.controller.js
+++ js/controller/establish.controller.js
@@ -1,19 +1,25 @@
-easyjob.controller('EstablishController', ['EstablishModel', '$scope', '$rootScope','MainModel',
+easyjob.controller('EstablishController', ['EstablishModel', '$scope', '$rootScope', 'MainModel',
     function (EstablishModel, $scope, $rootScope, MainModel) {
 
         $rootScope.headerDefault = false;
         $rootScope.headerDefaultLogout = true;
         $rootScope.footerDefault = false;
 
+        let sessionValidated = JSON.parse(sessionStorage.getItem('sessionValidated'));
+
+        $rootScope.name = sessionValidated != undefined ? sessionValidated.freelancer.name.split(" ")[0] : null;
+        $rootScope.id = sessionValidated != undefined ? sessionValidated.freelancer.id : null;
+        $rootScope.token = sessionValidated != undefined ? sessionValidated.token : null;
+
         // 
         // Buscar dados do estabelecimento
         // 
 
-        $scope.getEstablishment = function(){
+        $scope.getEstablishment = function () {
 
         }
 
-        
+
 
 
     }
