b2dbe7776d1cb48598c46362c4ddf240dd6c0369
xandrules <alexandresilva58@gmail.com>
2020-10-07 18:39:02 -0500

    deploy

--- js/factories/freelancer.factory.js
+++ js/factories/freelancer.factory.js
@@ -2,34 +2,28 @@ easyjob.factory('FreelancerModel', [
   'config',
   '$http',
   '$rootScope',
-  function (config, $http ,$rootScope) {
-
+  function (config, $http, $rootScope) {
     let token = $rootScope.token;
 
-      axios.interceptors.request.use(
-        config =>{
-          config.headers.authorization = token;
-          return config;
-        },
-        error =>{
-          return Promisse.reject(error);
-        }
-      );
-
+    axios.interceptors.request.use(
+      (config) => {
+        config.headers.authorization = token;
+        return config;
+      },
+      (error) => {
+        return Promisse.reject(error);
+      }
+    );
 
     var FreelancerFactory = {};
 
-    FreelancerFactory.getFreelancerById = function(id){
+    FreelancerFactory.getFreelancerById = function (id) {
+      return axios.get(config.baseUrl + `/freelancers/${id}`);
+    };
 
-     return axios.get(config.baseUrl + `/freelancers/${id}`);
-
-    }    
-
-    FreelancerFactory.updateFreelancer = function(data,id){
-
-      return axios.put(config.baseUrl + `/freelancers/${id}`,data);
- 
-     }    
+    FreelancerFactory.updateFreelancer = function (data, id) {
+      return axios.put(config.baseUrl + `/freelancers/${id}`, data);
+    };
 
     return FreelancerFactory;
   },
