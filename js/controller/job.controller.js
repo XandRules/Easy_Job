bab6e175cc149ec104cfa1b5a975995608d6066c
XandRules <alexandresilva58@gmail.com>
2020-10-14 14:46:10 -0300

    deploy

--- js/controller/job.controller.js
+++ js/controller/job.controller.js
@@ -12,17 +12,7 @@ easyjob.controller('JobController', [
     $rootScope.headerDefaultLogout = true;
     $rootScope.footerDefault = false;
 
-    $scope.dataFreelancer = {
-      "title": "profissional Garçom",
-      "description": "Tenho mais de 2 anos de experiência",
-      "period": "Manhã Tarde Noite",
-      "amount": "70",
-      "city": "Santa Rita do Sapucaí",
-      "day_of_week": "Domingo Segunda Terça Quarta Quinta Sexta Sábado",
-      "bio": "Experiência de mais de 2 anos como Garçom",
-      "name": "Alexandre da Silva Ribeiro",
-      "speciality_function": "Garçom/Garçonete"
-  }
+    $scope.dataFreelancer;
 
   $scope.dataFreelancer.day_of_week = $scope.dataFreelancer.day_of_week.split(" ");
   $scope.dataFreelancer.period = $scope.dataFreelancer.period.split(" ");
@@ -34,7 +24,7 @@ easyjob.controller('JobController', [
 
     $scope.pushAnnouncementFromFreelancer = () =>{
       $scope.loading = angular.element('#loading').addClass("loader loader-default is-active");
-      JobModel.getAnnouncementsFromFreelancer($rootScope.announcementSelectId).then(response =>{
+      JobModel.getAnnouncementsFromFreelancer($rootScope.announcementSelectId.anuncio_id).then(response =>{
         console.log(response);
         $scope.loading = angular.element('#loading').removeClass("loader loader-default is-active");
       });
