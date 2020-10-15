0a5a0103e0e06cef9076d3ac2632822cc912f558
XandRules <alexandresilva58@gmail.com>
2020-10-15 16:37:12 -0300

    up

--- js/controller/job.controller.js
+++ js/controller/job.controller.js
@@ -69,6 +69,39 @@ easyjob.controller('JobController', [
 
       console.log(data)
 
+      swal({
+        title: 'Submit your Github username',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Look up',
+        showLoaderOnConfirm: true,
+        preConfirm: (login) => {
+          return fetch(`//api.github.com/users/${login}`)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(response.statusText)
+              }
+              return response.json()
+            })
+            .catch(error => {
+              Swal.showValidationMessage(
+                `Request failed: ${error}`
+              )
+            })
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: `${result.value.login}'s avatar`,
+            imageUrl: result.value.avatar_url
+          })
+        }
+      })
+
       JobModel.createNotificationFreelancer(data).then(response =>{
         console.log(response.data)
       })
